8d09942e8fad282e47b3f5febfa4d18a447cc6ae
Jev <jev.kuznetsov@gmail.com>
2024-11-07 11:40:57 +0000

    add runners, rename cli

--- src/alpha_motion/cli.py
+++ src/alpha_motion/cli.py
@@ -4,7 +4,9 @@ alpha_motion CLI
 """
 
 import click
+
 from alpha_motion import __version__
+from alpha_motion.runners import run_main
 
 
 @click.group()
@@ -15,11 +17,31 @@ def cli() -> None:
 
 @cli.command()
 def info() -> None:
-    """ Print package info """
+    """Print package info"""
     print(__version__)
 
 
 cli.add_command(info)
+
+# run group
+
+
+@click.group()
+def run() -> None:
+    """Run commands"""
+    pass
+
+
+cli.add_command(run)
+
+
+@run.command()
+def mock_drives() -> None:
+    """Run mock drives"""
+    from alpha_motion.drives import mock_drives
+
+    run_main(mock_drives())
+
 
 if __name__ == "__main__":
     cli()  # pragma: no cover
---
+++ src/alpha_motion/runners.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Support functions
+
+Copyright (c) 2024 ROX Automation - Jev Kuznetsov
+"""
+
+import asyncio
+import logging
+import os
+from typing import Any, Coroutine, Callable, Union, TypeVar, cast
+
+import coloredlogs
+
+LOG_FORMAT = "%(asctime)s [%(name)s] %(filename)s:%(lineno)d - %(message)s"
+TIME_FORMAT = "%H:%M:%S.%f"
+
+# Type variable for the return type of the callable/coroutine
+T = TypeVar("T")
+
+# Define a type alias for our supported function types
+MainFunction = Union[
+    Callable[[], T],  # Regular synchronous function
+    Coroutine[Any, Any, T],  # Coroutine
+]
+
+
+def setup_logging() -> None:
+    """Setup logging"""
+    loglevel = os.environ.get("LOGLEVEL", "INFO").upper()
+    coloredlogs.install(level=loglevel, fmt=LOG_FORMAT, datefmt=TIME_FORMAT)
+    logging.info(f"Log level set to {loglevel}")
+
+
+def run_main(func: MainFunction[T], trace_on_exc=False) -> T | None:  # type: ignore
+    """
+    Convenience function to run either an async coroutine or a regular callable.
+
+    Args:
+        func: Either a coroutine or a regular callable to execute
+
+    Returns:
+        The return value of the executed function
+
+    Raises:
+        Exception: Any exception that occurs during execution (except KeyboardInterrupt)
+    """
+    setup_logging()
+
+    try:
+        if asyncio.iscoroutine(func):
+            # If it's a coroutine, run it with asyncio
+            return cast(T, asyncio.run(func))
+        else:
+            # If it's a regular callable, just call it
+            return cast(Callable[[], T], func)()
+    except KeyboardInterrupt:
+        logging.info("Process interrupted by user")
+        return cast(T, None)
+    except Exception as e:
+        logging.error(e, exc_info=trace_on_exc)
+        return cast(T, None)
