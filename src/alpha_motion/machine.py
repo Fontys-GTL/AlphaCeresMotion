7eb8afa36b67056c4073e25ccbd40d6daf776e46
Jev <jev.kuznetsov@gmail.com>
2024-11-07 12:33:08 +0000

    add roxbot req

---
+++ src/alpha_motion/machine.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+
+Main machine interface.
+
+inputs:  (velocity, curvature) over mqtt
+outputs: setpoints to left and right wheels over CAN
+
+Created on Thu Nov 07 2024
+
+Copyright (c) 2024 ROX Automation - Jev Kuznetsov
+"""
