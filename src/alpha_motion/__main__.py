a9c071af15563158344e780c2f4884b5d74dd304
Jev <jev.kuznetsov@gmail.com>
2024-11-07 21:26:48 +0000

    Refactor main entry point and integrate run_main function

--- src/alpha_motion/__main__.py
+++ src/alpha_motion/__main__.py
@@ -1,10 +1,15 @@
-""" main entry point for alpha_motion. """
+"""main entry point for alpha_motion."""
 
 from alpha_motion import __version__
+from alpha_motion.runners import run_main
+import alpha_motion.machine as machine
+
 
 def main() -> None:
     """main entry point for alpha_motion."""
-    print(f"Hello from alpha_motion version: {__version__}!")
+    print(f"alpha_motion version: {__version__}!")
+    run_main(machine.main())
+
 
 if __name__ == "__main__":
     main()
