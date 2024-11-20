635d1a7e0371571bd3cd71dbaab49f1b7d4febac
Jev <jev.kuznetsov@gmail.com>
2024-11-20 21:32:24 +0000

    update template

--- .devcontainer/init_container.sh
+++ .devcontainer/init_container.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Running init_container.sh, use this script to install libraries etc."
 
+set -e
 
 pre-commit install
+
+pip install -r .devcontainer/requirements.txt
 
 pip install -e .
